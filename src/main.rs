e38422025e28ac090e8726383d619577db0b519d
Hugo Acosta <thenex@gmail.com>
2023-09-18 16:20:33 -0400

    leyendo el input del usuario y mostrando el valor introducido

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    println!("Adivina el número elegido por Rust");
+    println!("--------------------");
+    println!("Por favor introduce un número entre el 1 y el 10");
+
+    let mut guess = String::new();
+
+    io::stdin()
+      .read_line(&mut guess)
+      .expect("Error al leer el número ingresado");
+
+    println!("El número que elegiste es: {guess}")
 }
