2ec16d87a97f4ce0dd5f18c31796a9d3a4d6e5c4
Hugo Acosta <thenex@gmail.com>
2023-09-19 17:50:22 -0400

    adding secret number and comparing them

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,14 @@
 use std::io;
+use rand::Rng;
+use std::cmp::Ordering;
 
 fn main() {
     println!("Adivina el número elegido por Rust");
+
+    let secret_number = rand::thread_rng().gen_range(1..=50);
+
     println!("--------------------");
-    println!("Por favor introduce un número entre el 1 y el 10");
+    println!("Por favor introduce un número entre el 1 y el 50");
 
     let mut guess = String::new();
 
@@ -11,5 +16,11 @@ fn main() {
       .read_line(&mut guess)
       .expect("Error al leer el número ingresado");
 
-    println!("El número que elegiste es: {guess}")
+    println!("El número que elegiste es: {guess}");
+
+    match guess.cmp(&secret_number) {
+      Ordering::Less => println!("Muy pequeño"),
+      Ordering::Greater => println!("Muy grande"),
+      Ordering::Equal => println!("Adivinaste!"),
+    }
 }
