a7da4d384171a117db172903b5dd0d3151d806da
Hugo Acosta <thenex@gmail.com>
2023-09-19 18:14:36 -0400

    guessing game finished

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,35 @@
 use std::io;
-use rand::Rng;
 use std::cmp::Ordering;
+use rand::Rng;
 
 fn main() {
     println!("Adivina el número elegido por Rust");
 
     let secret_number = rand::thread_rng().gen_range(1..=50);
+    loop {
+      println!("--------------------");
+      println!("Por favor introduce un número entre el 1 y el 50");
 
-    println!("--------------------");
-    println!("Por favor introduce un número entre el 1 y el 50");
+      let mut guess = String::new();
 
-    let mut guess = String::new();
+      io::stdin()
+        .read_line(&mut guess)
+        .expect("Error al leer el número ingresado");
 
-    io::stdin()
-      .read_line(&mut guess)
-      .expect("Error al leer el número ingresado");
+        let guess: u32 = match guess.trim().parse() {
+          Ok(num) => num,
+          Err(_) => continue,
+        };
 
-    println!("El número que elegiste es: {guess}");
+      println!("El número que elegiste es: {guess}");
 
-    match guess.cmp(&secret_number) {
-      Ordering::Less => println!("Muy pequeño"),
-      Ordering::Greater => println!("Muy grande"),
-      Ordering::Equal => println!("Adivinaste!"),
+      match guess.cmp(&secret_number) {
+        Ordering::Less => println!("Muy pequeño, intenta de nuevo"),
+        Ordering::Greater => println!("Muy grande, intenta de nuevo"),
+        Ordering::Equal => {
+          println!("Adivinaste!");
+          break;
+        }
+      }
     }
 }
